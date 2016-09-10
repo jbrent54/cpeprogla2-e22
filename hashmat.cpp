d071222945faa2cb4618b93630fface9738c1883
Brent Magdamit <jbrent56@gmail.com>
2016-09-10 11:47:08 +0800

    adds fizz buzz and hashmat

---
+++ hashmat.cpp
@@ -0,0 +1,15 @@
+//this program is created by brent
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+int ha, oa;
+
+	while(cin>>oa>>ha)
+	 cout << abs(oa-ha)<<endl;
+
+
+}
+

5bdc9253eb89d38f1e206cb72535adfe36cbdcbb
Brent Magdamit <jbrent56@gmail.com>
2016-09-17 12:22:12 +0800

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -51,3 +51,87 @@ void newLine(){
      char s;
      do{cin.get(s);}while(s!='\n');     
 }
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+struct Name{
+       char lastName[30];
+       char firstName[30];
+};
+
+struct Customer{
+       int contactno;
+       Name name;
+       double grade[3];       
+};
+struct Date{
+	int day;
+	int mos;
+	int year;
+};
+struct Items{
+	char name[20];
+	char id[20];
+	double price;
+	double quantity;
+};
+
+void newLine();
+
+main(){
+	Customer cust[5];
+	Date date;
+	Items items;
+	cout<<"Enter Three Customers"<<endl;
+	newLine();
+	for( int i=0;i<3; i++){
+		cout << "Customer Information "<<endl;
+		cout << "First Name: ";
+		cin.ignore();
+		cin.getline(cust[i].name.firstName,29);
+		cout << "Last Name: ";
+		cin.getline(cust[i].name.lastName,29);
+		cout << "Contact no.:";
+		cin >> cust[i].contactno;
+		cout << "Order Date:"<<endl;
+		cout << "Day: ";
+		cin  >> date.day;
+		cout << "Month: ";
+		cin  >> date.mos;
+		cout << "Year: ";
+		cin >> date.year;
+		cout <<"Enter 3 items:"<<endl;
+			for(int j=0; j<3;j++){
+				cout<<"ID: ";
+				cin >> items.id;
+				cin.ignore();
+				cout<<"Name: ";
+				cin.getline(items.name,19);
+				cout<<"Price: ";
+				cin >> items.price;
+				cout << "Quantity: ";
+				cin >> items.quantity;
+				cout << endl;
+				cout << endl;
+				cout << endl;
+			}
+		
+		
+	}
+	cout <<"/n/n";
+	cout <<"Summary Report"<<endl;
+	cout <<"#          " << "Customer Name       "<< "Order Date       " << "Item  " << "Price    " << "Quantity";
+	for( int i=0; i<3; i++){
+	cout<< " ";
+	cin >>cust[0].name.firstName;
+}
+}
+void newLine(){
+     char s;
+     do{cin.get(s);}while(s!='\n');     
+}

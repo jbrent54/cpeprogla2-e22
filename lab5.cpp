ac36605de44ee76cffe049f3b20dcd1727ad05c9
Brent Magdamit <jbrent56@gmail.com>
2016-09-23 22:35:50 +0800

    All problems compiled.

--- lab5.cpp
+++ lab5.cpp
@@ -1,137 +1,208 @@
+//this is a compilation of 3 problems of Lab 5
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+
+
+
 struct Name{
-       char lastName[30];
-       char firstName[30];
+       char fullName[100];
 };
 
 struct Student{
        int id;
        Name name;
-       double grade[3];       
+       double quiz[3]; 
+       double sum;
+	   double average;      
 };
 
-void newLine();
+struct Srecord{
+	int id;
+	char name[30];
+	struct Quizzes{
+		float quiz;
+	};
+ Quizzes quizzes[3];
+};
 
-int main(){
+
+struct Customer{
+	struct Name{
+		char firstName[20];
+		char lastName[20];
+	}name;
+	
+	struct Order{
+		int day;
+		int month;
+		int year;
+		int quan;
+		struct Item{
+			int ID;
+			char itemName[20];
+			int Price;
+		};
+	Item item[3];
+	}order;
+	
+	double contNo;
+};
+
+void NewLine(){
+	char s;
+	do{cin.get(s);}while(s!='\n');
+}
+
+void problem1(){
     Student stud;
-    double sum=0;
-    cout << "Enter student records: \n";
+    cout << "Enter Student Record: \n";
     cout << "ID: ";
     cin >> stud.id;
-    newLine();
-    cout << "First Name: ";
-    cin.getline(stud.name.firstName, 29);
-    cout << "Last Name: ";
-    cin.getline(stud.name.lastName, 29);
+    NewLine();
+    cout << "Name: ";
+    cin.getline(stud.name.fullName, 99);
     for(int i=0; i<3; i++){
-            cout << "Grade "<< i << ": ";
-            cin >> stud.grade[i];   
-			sum+= stud.grade[i];     
+            cout << "Quiz "<< i+1 << ": ";
+            cin >> stud.quiz[i];        
     }
     
     cout << "\n\n";
-    cout << "Display student records\n";
+    cout << "Student Record\n";
     cout << "ID: " << stud.id << endl;
-    cout << "First Name: " << stud.name.firstName << endl;
-    cout << "Last Name: " << stud.name.lastName << endl;
-    cout << "Average: "<<sum/3<<endl;
-    if(sum/3>75)
-    	cout<<"Remarks: Pass";
-    else
-    	cout<<"Remarks: Failed";
-			        
+    cout << "Name: " << stud.name.fullName << endl;
+    for(int i=0; i<3; i++){
+            cout << "Quiz "<< i+1 << ": " << stud.quiz[i] << endl;       
+            stud.sum+=stud.quiz[i];
+            stud.average=stud.sum/3;
+    }
+    cout<< "Grades: " << stud.average;
     
+    if (stud.average>=75)
+    {
+    	cout << " \nRemarks: Passed";
+	}
+	else
+	{
+		cout << " \nRemarks: Failed";
+	}
     
     system("pause > 0");
-    return 0;   
-}
-void newLine(){
-     char s;
-     do{cin.get(s);}while(s!='\n');     
 }
 
 
+void problem2(){
+	int i, j; 
+	float grade;
+	char p;
+	Srecord ci[5];
+	cout<<"Enter 5 Student<s> Record"<<endl;
+	for (i=0; i<5; i++){
+		cout<<"\nStudent "<<(i+1)<<endl;
+		cout<<"ID: ";
+		cin>>ci[i].id;
+		NewLine();
+		cout<<"Student Name: ";
+		cin.getline(ci[i].name, 49);
+		cout<<"Grades: ";
+		for(j=0;j<3;j++){	
+			cin>>ci[i].quizzes[j].quiz;	
+		}
+}
+	cout << setw(5) << "No."
+	<< setw(15)<<"Student No"
+	<< setw(20)<<"Name"
+	<< setw(10)<<"Grade"
+	<< setw(10)<<"Remark";
+	
 
-
-
-#include <iostream>
-using namespace std;
-
-struct Name{
-       char lastName[30];
-       char firstName[30];
-};
-
-struct Customer{
-       int contactno;
-       Name name;
-       double grade[3];       
-};
-struct Date{
-	int day;
-	int mos;
-	int year;
-};
-struct Items{
-	char name[20];
-	char id[20];
-	double price;
-	double quantity;
-};
-
-void newLine();
-
-main(){
-	Customer cust[5];
-	Date date;
-	Items items;
-	cout<<"Enter Three Customers"<<endl;
-	newLine();
-	for( int i=0;i<3; i++){
-		cout << "Customer Information "<<endl;
-		cout << "First Name: ";
-		cin.ignore();
-		cin.getline(cust[i].name.firstName,29);
-		cout << "Last Name: ";
-		cin.getline(cust[i].name.lastName,29);
-		cout << "Contact no.:";
-		cin >> cust[i].contactno;
-		cout << "Order Date:"<<endl;
-		cout << "Day: ";
-		cin  >> date.day;
-		cout << "Month: ";
-		cin  >> date.mos;
-		cout << "Year: ";
-		cin >> date.year;
-		cout <<"Enter 3 items:"<<endl;
-			for(int j=0; j<3;j++){
-				cout<<"ID: ";
-				cin >> items.id;
-				cin.ignore();
-				cout<<"Name: ";
-				cin.getline(items.name,19);
-				cout<<"Price: ";
-				cin >> items.price;
-				cout << "Quantity: ";
-				cin >> items.quantity;
-				cout << endl;
-				cout << endl;
-				cout << endl;
+	
+	for(int i=0; i<5; i++){
+		grade=0;
+		for(j=0;j<3;j++){
+		grade+=ci[i].quizzes[j].quiz;
+		}
+		cout<<endl;
+		cout<<setw(5)<<i+1
+		<<setw(15)<<ci[i].id
+		<<setw(20)<<ci[i].name;
+		cout<<setw(10)<<setprecision(4)<<grade/3<<setw(10);
+		if( grade/3>=75 ){
+			cout<<"Passed";
 			}
+			else{
+			cout<<"Failed";
+			}
+		}
+		cout<<endl;
+		system ("pause");
 		
-		
+	
 	}
-	cout <<"/n/n";
-	cout <<"Summary Report"<<endl;
-	cout <<"#          " << "Customer Name       "<< "Order Date       " << "Item  " << "Price    " << "Quantity";
-	for( int i=0; i<3; i++){
-	cout<< " ";
-	cin >>cust[0].name.firstName;
+	
+	
+int main(){
+	int i, j, tPrice;
+	problem1();
+	cout<<endl;
+	problem2();
+	cout<<endl;
+	Customer c[3];
+	for (i=0;i<3;i++){
+		cout<<"CUSTOMER INFORMATION "<<i+1<<endl;
+		cout<<"First Name: ";
+		cin.getline(c[i].name.firstName,29);
+		cout<<"Last Name: ";
+		cin.getline(c[i].name.lastName,29);
+		cout<<"ORDER DATE: "<<endl;
+		cout<<"Day: ";
+		cin>>c[i].order.day;
+		cout<<"Month: ";
+		cin>>c[i].order.month;
+		cout<<"Year: ";
+		cin>>c[i].order.year;
+		cout<<"\nENTER 3 ITEMS: "<<endl;
+		for (j=0;j<3;j++){
+		    cout<<"ID: ";
+		    cin>>c[i].order.item[j].ID;
+		    NewLine();
+    	    cout<<"Name: ";
+    		cin.getline(c[i].order.item[j].itemName,29);
+    		cout<<"Price: ";
+    		cin>>c[i].order.item[j].Price;
+    		cout<<"Quantity: ";
+    		cin>>c[i].order.quan;
+    		cout<<endl;
+		}
+		NewLine();
 }
+	j=0;
+	cout<<"SUMMARY REPORT"<<endl;
+	cout<<" #"<<setw(30)<<"Customer Name"
+	<<setw(20)<<"Order Date"
+	<<setw(15)<<"Items"
+	<<setw(15)<<"Price"
+	<<setw(15)<<"Quantity"<<endl;
+	
+	for (i=0;i<3;i++){
+		tPrice=0;
+		cout<<" "<<i+1
+		<<setw(23)<<c[i].name.lastName<<","
+		<<setw(6)<<c[i].name.firstName
+		<<setw(11)<<c[i].order.day<<"/"<<c[i].order.month<<"/"<<c[i].order.year<<endl;
+		
+    	for (j=0;j<3;j++){
+    		cout<<setw(67)<<c[i].order.item[j].itemName
+			<<setw(15)<<c[i].order.item[j].Price
+			<<setw(15)<<c[i].order.quan<<endl;
+    		tPrice += c[i].order.item[j].Price;
+		}
+		tPrice=tPrice*c[i].order.quan;
+		cout<<setw(96)<<"TOTAL PRICE: "<<tPrice<<endl;
+	}
+	
+	return 0;
+	
 }
-void newLine(){
-     char s;
-     do{cin.get(s);}while(s!='\n');     
-}
+
